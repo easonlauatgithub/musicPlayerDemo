9447a36f7db39bf37c61821290d16210e261ae1a
Eason Lau <easonlaulearningios@gmail.com>
2020-05-25 15:51:30 +0800

    Initial Commit

---
+++ lz_MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lz_MusicPlayer
+//
+//  Created by Eason on 25/5/2020.
+//  Copyright © 2020 cs50. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
